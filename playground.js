a94bacc3679216fe15e487f04bb836cbe0c12d6f
Anupam Dash <anupam.dash0505@gmail.com>
2021-04-06 12:00:13 +0530

    .

---
+++ playground.js
@@ -0,0 +1,70 @@
+let bgCol = 160;
+let cBut, sqBut, relBut;
+
+let circlesSet = [];
+let squaresSet = [];
+
+function setup(){
+    createCanvas(windowWidth, windowHeight).id("pg");
+    background(bgCol);
+    cBut = createButton("Circle");
+    sqBut = createButton("Square");
+    relBut = createButton("Reload Page");
+}
+
+function draw(){
+
+    background(bgCol);
+    ellipse(mouseX, mouseY, 50 * 2);
+
+    cBut.mousePressed(function() {
+       temp = new Circles; 
+       circlesSet.push(temp);
+    });
+
+    sqBut.mousePressed(function() {
+        temp = new Squares; 
+        squaresSet.push(temp);
+     });
+
+    relBut.mousePressed(function() {
+        location.reload();
+     });
+
+
+    for (let i = 0; i < circlesSet.length; i++)
+        circlesSet[i].show();
+
+    for (let i = 0; i < squaresSet.length; i++)
+        squaresSet[i].show();
+}
+
+class Circles{
+    constructor(){
+        this.x = random(width);
+        this.y = random(height);
+
+        this.r = random(10, 50);
+
+    }
+
+    show(){
+        ellipse(this.x, this.y, 2 * this.r);
+    }
+
+
+}
+
+class Squares {
+    constructor(){
+        this.x = random(width);
+        this.y = random(height);
+
+        this.r = random(10, 50);
+
+    }
+
+    show() {
+        rect(this.x, this.y, this.r, this.r);
+    }
+}
